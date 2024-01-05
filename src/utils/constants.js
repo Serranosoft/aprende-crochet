3dc1f66ac62c891cd09d74b9d388d50ffb6508ad
Manuel Scholz <manuel.serrano943@gmail.com>
2024-01-05 13:17:21 +0100

    Feat: Actualización de ads codes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const bannerId = "ca-app-pub-3963345159052388/5982467415";
-export const intersitialId = "ca-app-pub-3963345159052388/2122015979"
+export const bannerId = "ca-app-pub-3738413299329691/5033772583";
+export const intersitialId = "ca-app-pub-3738413299329691/2407609246"
