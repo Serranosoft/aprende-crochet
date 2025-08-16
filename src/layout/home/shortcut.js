bb7dc2aa84f3908745ec6c97a7190b7a945efe92
Manuel Scholz <manuel.serrano943@gmail.com>
2025-08-16 19:37:06 +0200

    Fix: Mostrar cantidad de tutoriales en los shortcuts

--- src/layout/home/shortcut.js
+++ src/layout/home/shortcut.js
@@ -3,14 +3,30 @@ import { colors, ui } from "../../utils/styles";
 import { router } from "expo-router";
 import { useEffect, useState } from "react";
 import { getPatternsInProgress } from "../../utils/sqlite";
+import stitchings from "../../../stitchings.json";
+import designs from "../../../designs.json";
 
 export default function Shortcut() {
 
+    const [patternsQty, setPatternsQty] = useState(null);
+    const [designsQty, setDesignsQty] = useState(null);
     const [patternsInProgress, setPatternsInProgress] = useState(0);
 
     useEffect(() => {
+        handlePatternsQty();
+        handleDesignsQty();
         handlePatternsInProgress();
     }, [])
+
+    async function handlePatternsQty() {
+        const result = stitchings.stitching.length;
+        setPatternsQty(result);
+    }
+
+    async function handleDesignsQty() {
+        const result = designs.designs.length;
+        setDesignsQty(result);
+    }
 
     async function handlePatternsInProgress() {
         const result = await getPatternsInProgress();
@@ -28,14 +44,14 @@ export default function Shortcut() {
                     <Image source={require("../../../assets/tutorials.png")} style={styles.icon} />
                     <View style={styles.info}>
                         <Text style={[ui.text, ui.white, ui.bold]}>Aprender desde cero</Text>
-                        <Text style={[ui.muted, ui.lightgray]}>12 tutoriales</Text>
+                        { patternsQty && <Text style={[ui.muted, ui.lightgray]}>{patternsQty} tutoriales</Text> }
                     </View>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.box} onPress={() => router.push("/")}>
                     <Image source={require("../../../assets/designs.png")} style={styles.icon} />
                     <View style={styles.info}>
                         <Text style={[ui.text, ui.white, ui.bold, { flex: 1, flexWrap: "wrap" }]}>Comenzar a diseñar</Text>
-                        <Text style={[ui.muted, ui.lightgray]}>21 diseños</Text>
+                        { designsQty && <Text style={[ui.muted, ui.lightgray]}>{designsQty} diseños</Text> }
                     </View>
                 </TouchableOpacity>
             </View>
@@ -73,7 +89,6 @@ const styles = StyleSheet.create({
     box: {
         flexDirection: "row",
         borderRadius: 8,
-        borderBottomLeftRadius: 0,
         padding: 12,
         gap: 8,
         backgroundColor: colors.box,
